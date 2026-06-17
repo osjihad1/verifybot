64c90a3366bbff77d165e6ed5a89f99ce5a9f63f
osjihad1 <120362880+osjihad1@users.noreply.github.com>
2026-06-17 21:41:30 +0600

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ async def verify(interaction: discord.Interaction):
 # --- Self-Ping to prevent Render sleep ---
 async def self_ping():
     await bot.wait_until_ready()
-    url = "https://verifybot-shjs.onrender.com"
+    url = "https://verifybot-shjs.onrender.com/"
     while not bot.is_closed():
         try:
             async with aiohttp.ClientSession() as session:
